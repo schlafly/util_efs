1e43a2ef844476dcb2d38be5ccdee29c05ba69d7
Eddie Schlafly <schlafly@hotmail.com>
2014-05-06 07:06:37 -0400

    Ugly hack to turn off omp.

---
+++ python/_omp.py
@@ -0,0 +1,52 @@
+def omp_set_num_threads(*args):
+  return 1
+
+def omp_set_dynamic(*args):
+  return 1
+
+def omp_set_nested(*args):
+  return 1
+
+def omp_set_max_active_levels(*args):
+  return 1
+
+def omp_get_num_threads():
+  return 1
+
+def omp_get_dynamic():
+  return 1
+
+def omp_get_nested():
+  return 1
+
+def omp_get_max_threads():
+  return 1
+
+def omp_get_thread_num():
+  return 1
+
+def omp_get_num_procs():
+  return 1
+
+def omp_in_parallel():
+  return 1
+
+def omp_get_active_level():
+  return 1
+
+def omp_get_level():
+  return 1
+
+def omp_get_ancestor_thread_num(*args):
+  return 1
+
+def omp_get_team_size(*args):
+  return 1
+
+def omp_get_thread_limit():
+  return 1
+
+def omp_get_max_active_levels():
+  return 1
+
+
